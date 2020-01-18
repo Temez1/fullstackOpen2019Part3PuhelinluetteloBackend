bdc83f494a7fe8aa41d523362849b68596e2e8eb
Temez1 <heleniusteemu@gmail.com>
2020-01-18 12:49:48 +0200

    uibuild

---
+++ build/precache-manifest.3dd47d2eaccb1325a896a2e14c28bd98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b180c21b297e643d7528b772d3673a0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001061b43438f3007cd2",
+    "url": "/static/js/2.0e6ac768.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0e6ac768.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56c48da77e722ad1a977",
+    "url": "/static/js/main.9667b2d3.chunk.js"
+  },
+  {
+    "revision": "4058619cfd054eca1abc",
+    "url": "/static/js/runtime-main.43089770.js"
+  }
+]);
