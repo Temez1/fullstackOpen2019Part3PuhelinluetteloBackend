e3e8f92deb39dc0419afe9d3e8ebec21feb3a7a6
Temez1 <heleniusteemu@gmail.com>
2020-01-21 17:33:29 +0200

    Modified console log error message

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ const unknownEndpoint = (req, res) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error)
+  console.error(error.message)
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return res.status(400).send({ error: 'malformatted id' })
