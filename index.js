28c5acde8979336719bce78eec36e95511cdbd83
Temez1 <heleniusteemu@gmail.com>
2020-01-21 18:57:11 +0200

    Added eslint and airbnb config + own config

--- index.js
+++ index.js
@@ -1,94 +1,94 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+require("dotenv").config()
+const express = require("express")
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const cors = require("cors")
 
-const personModel = require('./models/person')
+const personModel = require("./models/person")
 
 const app = express()
 
-app.use(express.static('build'))
+app.use(express.static("build"))
 app.use(bodyParser.json())
 
-morgan.token('res-body', (req, res) => {
+morgan.token("res-body", (req) => {
   const bodyString = JSON.stringify(req.body)
-  if (bodyString === '{}') {
+  if (bodyString === "{}") {
     // Returning space because otherwise morgan will return '-'
-    return(' ')
+    return (" ")
   }
   return bodyString
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res-body'))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :res-body"))
 app.use(cors())
 
-app.get('/api/persons', (req, res) => {
-  personModel.find({}).then(persons => {
-    res.json(persons.map(person => person.toJSON()))
+app.get("/api/persons", (req, res) => {
+  personModel.find({}).then((persons) => {
+    res.json(persons.map((person) => person.toJSON()))
   })
 })
 
-app.post('/api/persons', (req, res, next) => {
+app.post("/api/persons", (req, res, next) => {
   const requestedPerson = req.body
 
   const newPerson = new personModel({
-    name: requestedPerson.name ,
-    number: requestedPerson.number ,
+    name: requestedPerson.name,
+    number: requestedPerson.number,
   })
 
   newPerson.save()
-    .then(savedPerson => savedPerson.toJSON())
-    .then(savedAndFormattedPerson => {
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => {
       res.json(savedAndFormattedPerson)
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
-app.get('/api/persons/:id', (req, res, next) => {
-  personModel.findById(req.params.id).then(person => {
-    if (person){
+app.get("/api/persons/:id", (req, res, next) => {
+  personModel.findById(req.params.id).then((person) => {
+    if (person) {
       res.json(person.toJSON())
-    }
-    else {
+    } else {
       res.status(404).end()
     }
   })
-  .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
-app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+app.put("/api/persons/:id", (req, res, next) => {
+  const { body } = req
 
   const person = {
     name: body.name,
-    number: body.number
+    number: body.number,
   }
 
   // mongoose-unique-validator context option, more:
   // https://www.npmjs.com/package/mongoose-unique-validator#find--updates
-  personModel.findByIdAndUpdate(req.params.id, person, {new: true, runValidators:true, context:'query'})
-      .then(updatedPerson => {
-        // This happens when someone has deleted the doc and you are trying to
-        // update it (toJSON throws TypeError "can't read null")
-        if (updatedPerson === null){
-          return res.status(410).json({error:"The person information is deleted already from the server."})
-        } 
-        res.json(updatedPerson.toJSON())
-      })
-      .catch(error => next(error))
+  personModel.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: "query" })
+    .then((updatedPerson) => {
+      // This happens when someone has deleted the doc and you are trying to
+      // update it (toJSON throws TypeError "can't read null")
+      if (updatedPerson === null) {
+        return res.status(410).json({ error: "The person information is deleted already from the server." })
+      }
+      res.json(updatedPerson.toJSON())
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res, next) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   personModel.findByIdAndRemove(req.params.id)
-      .then(result => {
-        res.status(204).end()
-      })
-      .catch(error => next(error))
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({error: 'unknown endpoint'})
+  res.status(404).send({ error: "unknown endpoint" })
 }
 
 app.use(unknownEndpoint)
@@ -96,28 +96,28 @@ app.use(unknownEndpoint)
 const errorHandler = (error, req, res, next) => {
   console.error(error.message)
 
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return res.status(400).send({ error: 'malformatted id' })
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return res.status(400).send({ error: "malformatted id" })
   }
-  else if (error.name === 'ValidationError'){
+  if (error.name === "ValidationError") {
     // Person name
     if (error.errors.name) {
-      if (error.errors.name.kind === 'required') {
+      if (error.errors.name.kind === "required") {
         return res.status(400).json({ error: error.message })
       }
-      else if (error.errors.name.kind === 'minlength'){
+      if (error.errors.name.kind === "minlength") {
         return res.status(400).json({ error: error.message })
       }
-      else if (error.errors.name.kind === 'unique'){
+      if (error.errors.name.kind === "unique") {
         return res.status(409).json({ error: error.message })
       }
     }
     // Person number
-    if(error.errors.number){
-      if (error.errors.number.kind === 'required') {
+    if (error.errors.number) {
+      if (error.errors.number.kind === "required") {
         return res.status(400).json({ error: error.message })
       }
-      else if (error.errors.number.kind === 'minlength'){
+      if (error.errors.number.kind === "minlength") {
         return res.status(400).json({ error: error.message })
       }
     }
@@ -128,7 +128,7 @@ const errorHandler = (error, req, res, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const { PORT } = process.env
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
--- models/person.js
+++ models/person.js
@@ -1,34 +1,37 @@
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+const mongoose = require("mongoose")
+const uniqueValidator = require("mongoose-unique-validator")
 
-mongoose.set('useUnifiedTopology', true)
-mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true)
+mongoose.set("useUnifiedTopology", true)
+mongoose.set("useFindAndModify", false)
+mongoose.set("useCreateIndex", true)
 
 const dbUrl = process.env.MONGODB_URI
 
 console.log("Connecting to MongoDB")
 
-mongoose.connect(dbUrl, {useNewUrlParser: true})
-.then(result => {
+mongoose.connect(dbUrl, { useNewUrlParser: true })
+  .then(() => {
     console.log("Connected to MongoDB")
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     console.log("Error connecting to MongoDB", error.message)
-})
+  })
 
 const personSchema = new mongoose.Schema({
-  name: {type:String, minlength:3, required:true, unique:true},
-  number: {type:String, minlength:8, required:true}
+  // eslint-disable-next-line object-curly-newline
+  name: { type: String, minlength: 3, required: true, unique: true },
+  number: { type: String, minlength: 8, required: true },
 })
 personSchema.plugin(uniqueValidator)
 
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema)
